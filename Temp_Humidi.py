274ff2b987aa4cecab1f71ce84712c9f3f8ee2a8
gari30 <gariyoshi0630@gmail.com>
2021-07-01 03:21:22 +0900

    関数のコメントを追加

--- Temp_Humidi.py
+++ Temp_Humidi.py
@@ -11,6 +11,10 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 # firestoreにデータを上げる
+# param date: 日付データ(数値)
+# param temperature: 温度データ(数値)
+# param humidity: 湿度データ(数値)
+# param co2_concentration: CO2濃度(数値)
 def pushDataFirestore(date, temperature, humidity, co2_concentration):
   cred = credentials.Certificate('./kyokko-ob-team_firestore.json')
   firebase_admin.initialize_app(cred)
@@ -23,6 +27,16 @@ def pushDataFirestore(date, temperature, humidity, co2_concentration):
     'co2-concentration': co2_concentration,
   })
 
+# 温度データを数値変換
+def tempChanger(msb, lsb):
+  mlsb = ((msb << 8 ) | lsb)
+  return (-45 + 175 * int( str( mlsb ), 10) / (pow( 2, 16 ) - 1 ))
+
+# 湿度データを温度変換
+def humidChanger( msb, lsb):
+  mlsb = ((msb << 8) | lsb)
+  return ( 100 * int( str( mlsb ), 10) / (pow( 2, 16 ) - 1 ))
+
 now_month = datetime.datetime.now().strftime('%Y%m')
 Path = './' + now_month + '_Temp_Humidi_Sensor_Data.json'
 ###ファイルがなかったら作成
@@ -31,16 +45,7 @@ if not os.path.exists(Path):
   with open(Path, 'w'):
       pass
 
-def tempChanger(msb, lsb):
-  mlsb = ((msb << 8 ) | lsb)
-  return (-45 + 175 * int( str( mlsb ), 10) / (pow( 2, 16 ) - 1 ))
-
-def humidChanger( msb, lsb):
-  mlsb = ((msb << 8) | lsb)
-  return ( 100 * int( str( mlsb ), 10) / (pow( 2, 16 ) - 1 ))
-
 i2c = smbus.SMBus(1)
-
 i2c_addr = 0x45
 
 i2c.write_byte_data( i2c_addr, 0x21, 0x30)
